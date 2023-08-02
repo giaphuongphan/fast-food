52ac56fc9feb2e58b4b521e941ee98f24c1fc3a8
Minhhhh <mineskiroxor@gmail.com>
2023-08-03 00:51:02 +0800

    fix: clean up function call

--- kfc_scrapping.py
+++ kfc_scrapping.py
@@ -20,43 +20,43 @@ def write_to_csv(file_path: str,
     headers = data[list(data.keys())[0]].keys()
 
     with open(file=file_path, mode="w") as csv_file:
-        csv_writer = csv.DictWriter(csv_file, fieldnames=headers)
+        csv_writer = csv.DictWriter(f=csv_file, fieldnames=headers)
         csv_writer.writeheader()
 
         for food in data.keys():
-            csv_writer.writerow(data[food])
+            csv_writer.writerow(rowdict=data[food])
 
     csv_file.close()
 
 def main():
 
     session = HTMLSession()
-    result = session.get(KFC_URL)
+    result = session.get(url=KFC_URL)
     result.html.render()
 
     # Extract data from nutrion tables on kfc website
-    tables = result.html.find("table.table.table-bordered.tableChartNutrition.table-sm")
+    tables = result.html.find(selector="table.table.table-bordered.tableChartNutrition.table-sm")
     scraped_food = {}
 
     for table in tables:
-        table_body = table.find("tbody")[0]
-        table_rows = table_body.find("tr")
+        table_body = table.find(selector="tbody")[0]
+        table_rows = table_body.find(selector="tr")
 
         header = table_rows[0]
-        nutrients = header.find("th")
+        nutrients = header.find(selector="th")
 
         current_allergens = None
         if len(nutrients) >= NUM_NUTRIENTS:
             food_list = table_rows[1:]
             for idx, food in enumerate(food_list):
-                name = food.find("td")[0].text
+                name = food.find(selector="td")[0].text
 
                 if idx == 0:
-                    current_allergens_list = food.find("td")[1].find("ul")[0].find("li")
+                    current_allergens_list = food.find(selector="td")[1].find(selector="ul")[0].find(selector="li")
                     current_allergens = ",".join([allergen.text for allergen in current_allergens_list])
-                    weights =  [val.text for val in food.find("td")[2:9]]
+                    weights =  [val.text for val in food.find(selector="td")[2:9]]
                 else:
-                    weights = [val.text for val in food.find("td")[1:8]]
+                    weights = [val.text for val in food.find(selector="td")[1:8]]
 
                 scraped_food[name] = {
                     "Food": name,
@@ -72,7 +72,7 @@ def main():
 
 
             current_allergens = None
-    pprint.pprint(scraped_food)
+    pprint.pprint(object=scraped_food)
 
     # Write and save data to a kfc_data.csv file
     csv_file_path = f"{DATA_DIR}/kfc_data.csv"
@@ -87,8 +87,8 @@ def main():
             val += "g"
         new_servings.append(val)
 
-    new_column = pd.Series(new_servings, name='Servings (g or ml)')
-    df.update(new_column)
+    new_column = pd.Series(data=new_servings, name='Servings (g or ml)')
+    df.update(other=new_column)
 
     df.to_csv(path_or_buf=csv_file_path, index=False)
 
